d0297c97e5de049f1eaa48f9b962f481a714984c
vlhod <40525571+icft@users.noreply.github.com>
2021-10-27 18:34:31 +0300

    Update main.go

--- lab2/main.go
+++ lab2/main.go
@@ -1,15 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"lab2/AST"
-)
-
 func main() {
-	var s = "b|ca+(())(65:kodw)(f)b{1,2}a\\2(565:moemveo)"
-	str := AST.AddConcatenations(s)
-	tokens := AST.CreateTokens(str)
+	//var s = "(#|((6)2)b{1,2}a\\32(5:mo))"
+	//var s = "(#|b+ca|b{1,2})"
+	//var s = "(56:r|ek#|b{1,2})"
+	/*//var s = "((6:dfff)\\6c(ab|c))"
+	var s = "(b|ca+#|((36)2))"
+	var str = tree.AddConcatenations(s)
 	fmt.Println(str)
+	var tokens = tree.CreateTokens(str)
 	fmt.Println(tokens)
-	fmt.Println(AST.CreateRPN(tokens))
+	var nodes = tree.CreateNodes(tokens)
+	var first, second = tree.ClosestBrackets(nodes)
+	for second-first > 1 {
+		fmt.Println(first, second)
+		tree.Print(nodes)
+		copy(nodes[first:], nodes[second+1:])
+		nodes = nodes[:len(nodes)-(second-first+1)]
+		tree.Print(nodes)
+		fmt.Println()
+		first, second = tree.ClosestBrackets(nodes)
+	}*/
+	//nodes := AST.CreateNodes(tokens)
 }
