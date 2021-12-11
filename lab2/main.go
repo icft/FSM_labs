ca007b79f6e17fe3555a7ea6ec8c4be97f7bda19
vlhod <40525571+icft@users.noreply.github.com>
2021-12-11 17:49:41 +0300

    Update main.go

--- lab2/main.go
+++ lab2/main.go
@@ -1,37 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"lab2/dfa"
-	"lab2/tree"
+	"regex/dfa"
 )
 
 func main() {
-	var s = "(((a|b)abb)$)"
+	var s = "(((5:aas)aa)$)"
+	//var str = "((a)$)"
 	//var s = "(a|b+.(7:fg)\\7b{1,2}j)"
-	var str = tree.AddConcatenations(s)
-	fmt.Println(str)
-	var d = dfa.Compile(s)
-	dfa.Minimization(d)
-//	dfa.Search(s, "fepfk")
-	/*var regex = tree.ReplaceRepeat(str)
-	fmt.Println(regex)
-	var tokens = tree.CreateTokens(regex)
-	fmt.Println(tokens)
-	*///var root = tree.CreateTree(s)
-	/*fmt.Printf("\n\n\n")
-	tree.PrintTree(root)
-	var a []int = nil
-	var followpos [][]int
-	for i := 0; i < len(tree.ID); i++ {
-		followpos = append(followpos, a)
-	}
-	tree.FindFollowPos(root, followpos)
-	//fmt.Println(tree.FollowPos)
-	fmt.Printf("\n\n\n\n")*/
-	//fmt.Println(root.FirstPos)
-	//var avtomata = dfa.InitDFA(root, tree.FollowPos, root.FirstPos)
-	//var state = dfa.Convert(avtomata, root)
-	//fmt.Println("\n", "\n", state, "\n\n")
-	//dfa.Print(avtomata)
+	//fmt.Println(tree.ReplaceRepeat(tree.AddConcatenations(s)))
+	//dfa.Minimization(d)
+	dfa.Compile(s)
+	//var d2 = dfa.Compile(str)
+	//var m = dfa.GetTransitions(d.InitialState, nil)
+	//fmt.Println(dfa.CreateRE(dfa.CopyDFA(d), d.InitialState, m))
+	//fmt.Println(dfa.Difference(d,d2))
+	//	dfa.Search(s, "fepfk")
 }
+
