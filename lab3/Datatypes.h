f8d7d6b2c13df284f6b9028df8a7ffef3796fb40
vlhod <40525571+icft@users.noreply.github.com>
2021-12-29 22:23:57 +0300

    Update Datatypes.h

--- lab3/Datatypes.h
+++ lab3/Datatypes.h
@@ -1,213 +1,234 @@
 #pragma once
-#ifndef INTERPRETER_DATATYPES_H
-#define INTERPRETER_DATATYPES_H
 
 #include <iostream>
 #include <list>
 #include <utility>
 #include <vector>
 #include <numeric>
+#include <exception>
+#include "Errors.h"
 
-enum class DataTypes { INT, SHORT, BOOL, VECTOR };
-enum class Logic { TRUE, FALSE, UNDEFINED };
+enum class Datatypes {INT, SHORT, BOOL, VECTOR};
+enum class Logic {TRUE, FALSE, UNDEFINED};
+
 class Object {
 public:
-    virtual ~Object() = 0;
-    [[nodiscard]] virtual DataTypes get_type() const noexcept = 0;
-    virtual std::shared_ptr<Object> copy() = 0;
-    virtual bool operator==(std::shared_ptr<Object>) = 0;
-    virtual explicit operator int() = 0; //написать ошибку
-    virtual Logic to_bool() = 0; //написать ошибку
+	virtual ~Object() = default;
+	virtual Datatypes get_type() = 0;
+	virtual bool operator==(std::shared_ptr<Object>) = 0;
+	virtual void operator=(std::shared_ptr<Object>) = 0;
+	virtual operator int() {
+		throw CastError("Can't convert to int");
+	}
+	virtual operator short() {
+		throw CastError("Can't convert to short");
+	}
+	virtual Logic to_bool() {
+		throw CastError("Can't convert to bool");
+	}
 };
 
 class Int : public Object {
-private:
-    int value;
-    DataTypes type = DataTypes::INT;
 public:
-    Int() : value(0) {}
-    explicit Int(int val) : value(val) {}
-    Int(Int& i) : value(i.value) {}
-    std::shared_ptr<Object> copy() override {
-        return std::make_shared<Int>(value);
-    }
-    [[nodiscard]] DataTypes get_type() {
-        return type;
-    }
-    bool operator==(Int& i) const {
-        if (value == i.value) {
-            return true;
-        }
-        return false;
-    }
-    explicit operator int() override {
-        return value;
-    }
-    Logic to_bool() override {
-        if (value > 0) {
-            return Logic::TRUE;
-        }
-        else if (value < 0) {
-            return Logic::FALSE;
-        }
-        else {
-            return Logic::UNDEFINED;
-        }
-    }
-    ~Int() override = default;
+	std::int32_t value;
+	Datatypes type = Datatypes::INT;
+
+	Int() : value(0) {}
+	Int(std::int32_t v) : value(v) {}
+	Int(Int& i) : value(i.value) {}
+	virtual ~Int() = default;
+	virtual Datatypes get_type() {
+		return type;
+	}
+	virtual bool operator==(std::shared_ptr<Object> obj) {
+		if (value == (int)*obj) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	virtual void operator=(std::shared_ptr<Object> obj) {
+		value = (int)*obj;
+	}
+	virtual operator int() {
+		return value;
+	}
+	virtual operator short() {
+		return (std::int16_t)value;
+	}
+	virtual Logic to_bool() {
+		if (value > 0) {
+			return Logic::TRUE;
+		}
+		else if (value < 0) {
+			return Logic::FALSE;
+		}
+		else {
+			return Logic::UNDEFINED;
+		}
+	}
 };
 
 class Short : public Object {
-private:
-    short value;
-    DataTypes type = DataTypes::SHORT;
 public:
-    Short() : value(0) {}
-    explicit Short(short val) : value(val) {}
-    Short(Short& s) : value(s.value) {}
-    Short(Short&& s) noexcept : value(s.value) {}
-    std::shared_ptr<Object> copy() override {
-        return std::make_shared<Short>(value);
-    }
-    [[nodiscard]] DataTypes get_type() {
-        return type;
-    }
-    bool operator==(Short& s) const {
-        if (value == s.value) {
-            return true;
-        }
-        return false;
-    }
-    explicit operator int() override {
-        return value;
-    }
-    Logic to_bool() override {
-        if (value > 0) {
-            return Logic::TRUE;
-        }
-        else if (value < 0) {
-            return Logic::FALSE;
-        }
-        else {
-            return Logic::UNDEFINED;
-        }
-    }
-    ~Short() override = default;
+	std::int16_t value;
+	Datatypes type = Datatypes::SHORT;
+
+	Short() : value(0) {}
+	Short(std::int16_t v) : value(v) {}
+	Short(Short& i) : value(i.value) {}
+	virtual ~Short() = default;
+	virtual Datatypes get_type() {
+		return type;
+	}
+	virtual bool operator==(std::shared_ptr<Object> obj) {
+		if (value == (int)*obj) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	virtual void operator=(std::shared_ptr<Object> obj) {
+		value = (short)*obj;
+	}
+	virtual operator int() {
+		return (std::int32_t)value;
+	}
+	virtual operator short() {
+		return value;
+	}
+	virtual Logic to_bool() {
+		if (value > 0) {
+			return Logic::TRUE;
+		}
+		else if (value < 0) {
+			return Logic::FALSE;
+		}
+		else {
+			return Logic::UNDEFINED;
+		}
+	}
 };
 
 class Bool : public Object {
-private:
-    Logic value;
-    DataTypes type = DataTypes::BOOL;
 public:
-    Bool() : value(Logic::UNDEFINED) {}
-    explicit Bool(Logic val) : value(val) {}
-    Bool(Bool& b) : value(b.value) {}
-    Bool(Bool&& b) noexcept : value(b.value) {}
-    std::shared_ptr<Object> copy() override {
-        return std::make_shared<Bool>(value);
-    }
-    [[nodiscard]] DataTypes get_type() {
-        return type;
-    }
-    bool operator==(Bool& b) const {
-        if (value == b.value) {
-            return true;
-        }
-        return false;
-    }
-    explicit operator int() override {
-        if (value == Logic::TRUE) {
-            return 1;
-        }
-        else if (value == Logic::FALSE) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-    Logic to_bool() override {
-        return value;
-    }
-    ~Bool() override = default;
+	Logic value;
+	Datatypes type = Datatypes::BOOL;
+
+	Bool() : value(Logic::UNDEFINED) {}
+	Bool(Logic v) : value(v) {}
+	Bool(Bool& i) : value(i.value) {}
+	virtual ~Bool() = default;
+	virtual Datatypes get_type() {
+		return type;
+	}
+	virtual bool operator==(std::shared_ptr<Object> obj) {
+		if (value == obj->to_bool()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	virtual void operator=(std::shared_ptr<Object> obj) {
+		value = obj->to_bool();
+	}
+	virtual operator int() {
+		if (value == Logic::TRUE) {
+			return 1;
+		}
+		else if (value == Logic::FALSE) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	virtual operator short() {
+		if (value == Logic::TRUE) {
+			return 1;
+		}
+		else if (value == Logic::FALSE) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	virtual Logic to_bool() {
+		return value;
+	}
 };
+
 
 class Vector : public Object {
-private:
-    std::vector<std::shared_ptr<Object>> vec;
-    std::vector<int> dim;
-    DataTypes type = DataTypes::VECTOR;
 public:
-    Vector() = default;
-    Vector(std::initializer_list<int> dims) {
-        auto count = std::accumulate(std::begin(dims), std::end(dims), 1, std::multiplies());
-        if (count < 0) {
-            throw std::overflow_error("Overflow");
-        }
-        for (int i = 0; i < count; i++) {
-            vec.push_back(std::make_shared<Object>());
-        }
-        dim = dims;
-    }
-    explicit Vector(const std::vector<std::shared_ptr<Object>>& v) {
-        for (auto i : v) {
-            vec.push_back(i);
-        }
-    }
-    explicit Vector(const std::vector<std::shared_ptr<Object>>& v, const std::vector<int>& dims) {
-        dim = dims;
-        for (auto i : v) {
-            vec.push_back(i);
-        }
+	std::vector<std::shared_ptr<Object>> vec;
+	std::vector<int> dimentions;
+	Datatypes type = Datatypes::VECTOR;
 
-    }
-    Vector(Vector& v) {
-        for (auto i : v.vec) {
-            vec.push_back(i);
-        }
-        dim = v.dim;
-    }
-    Vector(Vector&& v)  noexcept : vec(std::move(v.vec)), dim(std::move(v.dim)) {}
-    std::shared_ptr<Object> copy() override {
-        return std::make_shared<Vector>(vec);
-    }
-    bool operator==(Vector& v) const {
-        if (v.vec.size() != vec.size()) {
-            return false;
-        }
-        if (v.dim.size() != dim.size()) {
-            return false;
-        }
-        for (auto i = 0; i < dim.size(); i++) {
-            if (!(v.dim[i] == v.dim[i])) {
-                return false;
-            }
-        }
-        for (auto i = 0; i < vec.size(); i++) {
-            if (!(v.vec[i] == v.vec[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-    [[nodiscard]] DataTypes get_type() {
-        return type;
-    }
-    std::shared_ptr<Object> operator[](std::vector<int> dims) {
-        std::vector<int> s;
-        for (auto i = 0; i < dim.size(); i++) {
-            s.push_back(1);
-            for (auto j = i + 1; j < dim.size(); j++) {
-                s[i] *= dim[j];
-            }
-        }
-        int index = 0;
-        for (auto i = 0; i < s.size(); i++) {
-            index += s[i] * dims[i];
-        }
-        return vec[index];
-    }
-    ~Vector() override = default;
+	Vector(std::vector<int> dims) {
+		auto count = 1;
+		for (auto it : dims) {
+			if (it < 1)
+				throw IndexError("Index can't be negative");
+			count *= dims[it];
+		}
+		//auto count = std::accumulate(std::begin(dims), std::end(dims), 1, std::multiplies<int>());
+		if (count < 0) {
+			throw OverflowError("Int overflow");
+		}
+		for (int i = 0; i < count; i++) {
+			vec.push_back(std::make_shared<Object>());
+		}
+	}
+	Vector(const std::vector<std::shared_ptr<Object>> v) {
+		for (auto it : v) {
+			vec.push_back(it);
+		}
+	}
+	Vector(Vector&& v) : vec(std::move(v.vec)), dimentions(std::move(v.dimentions)) {}
+	virtual bool operator==(Vector& v) {
+		if (v.vec.size() != vec.size()) {
+			return false;
+		}
+		if (v.dimentions.size() != dimentions.size()) {
+			return false;
+		}
+		for (auto i = 0; i < dimentions.size(); i++) {
+			if (!(v.dimentions[i] == v.dimentions[i])) {
+				return false;
+			}
+		}
+		for (auto i = 0; i < vec.size(); i++) {
+			if (!(v.vec[i] == v.vec[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+	virtual void operator=(std::shared_ptr<Object> obj) {
+		auto v = std::dynamic_pointer_cast<Vector>(obj);
+		vec = v->vec;
+		dimentions = v->dimentions;
+	}
+	virtual Datatypes get_type() {
+		return type;
+	}
+	std::shared_ptr<Object> operator[](std::vector<int> dims) {
+		std::vector<int> s;
+		for (auto i = 0; i < dimentions.size(); i++) {
+			s.push_back(1);
+			for (auto j = i + 1; j < dimentions.size(); j++) {
+				s[i] *= dimentions[j];
+			}
+		}
+		int index = 0;
+		for (auto i = 0; i < s.size(); i++) {
+			index += s[i] * dims[i];
+		}
+		return vec[index];
+	}
+	virtual ~Vector() = default;
 };
-#endif
